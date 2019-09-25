6d3ecdccbc19fe9e5a74a8ee3a3b80eea624b9b4
antonpurnama21 <41947166+antonpurnama21@users.noreply.github.com>
2019-09-25 17:34:14 +0700

    Update footer.php

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -1,3 +1,3 @@
 <div class="footer text-muted">
-	Copyright &copy; 2015. <a href="#">Intern </a> CBN <a href="http://themeforest.net/user/Kopyov" target="_blank">R.A.W</a>
+	Copyright &copy; 2015. <a href="#">Intern </a> CBN <a href="http://themeforest.net/user/Kopyov" target="_blank">A.P</a>
 </div>
