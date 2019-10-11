be32bf6da6894d271039076459f6cfa890b03664
antonpurnama21 <antonpurnama9878@gmail.com>
2019-10-11 16:33:04 +0700

    merubah admin.php

--- application/views/pages/admin/index.php
+++ application/views/pages/admin/index.php
@@ -15,9 +15,9 @@
 	<table class="table datatable-responsive-row-control table-hover">
 		<thead>
 			<tr style="font-size:12px;text-align:center;">
-				<th width="10%">No</th>
+				<th>No</th>
 				<th>Information Admin Campus</th>
-				<th width="20%">Action</th>
+				<th>Action</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -61,9 +61,9 @@
 					 </div>
 				</td>
 				<td>
-					<a data-placement="left" data-popup="tooltip" title="Reset Password" style="margin-bottom: 5px" onclick="showModal('<?= base_url("admin/modalReset")?>', '<?= $key->adminID.'~'.$key->fullName?>', 'resetpass')"><i class="icon-lock"></i></a>
-					<a data-placement="left" data-popup="tooltip" title="Edit Data" style="margin-bottom: 5px" onclick="showModal('<?=base_url("admin/modalEdit")?>', '<?=$key->adminID.'~'.$key->fullName?>', 'editadmin')"><i class="icon-quill4"></i></a>
-					<a data-placement="left" data-popup="tooltip" title="Delete Data" style="margin-bottom: 5px; color: red;" onclick="confirms('Delete','Admin `<?= $key->fullName?>`?','<?= base_url("admin/delete")?>','<?= $key->adminID?>')"><i class="icon-trash"></i></a>
+					<a data-placement="left" data-popup="tooltip" title="Reset Password" style="margin: 10px" onclick="showModal('<?= base_url("admin/modalReset")?>', '<?= $key->adminID.'~'.$key->fullName?>', 'resetpass')"><i class="icon-lock"></i></a>
+					<a data-placement="left" data-popup="tooltip" title="Edit Data" style="margin: 10px" onclick="showModal('<?=base_url("admin/modalEdit")?>', '<?=$key->adminID.'~'.$key->fullName?>', 'editadmin')"><i class="icon-quill4"></i></a>
+					<a data-placement="left" data-popup="tooltip" title="Delete Data" style="margin: 10px; color: red;" onclick="confirms('Delete','Admin `<?= $key->fullName?>`?','<?= base_url("admin/delete")?>','<?= $key->adminID?>')"><i class="icon-trash"></i></a>
 				</td>
 			</tr>
 			<?php
