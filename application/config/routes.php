4eea0f32682560dd47298991bda128011e4d800e
antonpurnama21 <antonpurnama9878@gmail.com>
2019-10-27 22:34:26 +0700

    perubahan yang belum di push

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,4 +51,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'dashboard';
 $route['404_override'] = '';
+//$route['(:any)'] = 'welcome/$1';
 $route['translate_uri_dashes'] = FALSE;
