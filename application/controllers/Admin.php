d941162e9cf3660ca1fe62f5d0e52d304beaff2c
antonpurnama21 <antonpurnama9878@gmail.com>
2019-11-08 17:43:20 +0700

    modified scroll table for log activity in profile

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -376,7 +376,7 @@ class Admin extends CommonDash {
 			'_JS' => generate_js(array(
 						"dashboards/js/plugins/ui/moment/moment.min.js",
 						"dashboards/js/plugins/tables/datatables/datatables.min.js",
-						"dashboards/js/plugins/tables/datatables/extensions/responsive.min.js",
+						"dashboards/js/plugins/tables/datatables/extensions/scroller.min.js",
 						"dashboards/js/plugins/forms/selects/select2.min.js",
 						"dashboards/js/pages/datatables_responsive.js",
 						"dashboards/js/plugins/forms/styling/switch.min.js",
